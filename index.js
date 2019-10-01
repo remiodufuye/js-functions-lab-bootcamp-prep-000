376a90adc3b5c3dd91775d58b002a50a3075fad4
remi.odufuye <remi.odufuye@gmail.com>
2019-10-01 17:10:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays() {
   return "Happy holidays!" ; 
 }
 
-function happyHolidayTo(holiday , name) {
+function happyHolidayTo(holiday,name) {
   return `Happy ${holiday}, ${name}!` ; 
 }
 
